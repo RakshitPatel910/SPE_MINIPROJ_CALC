ed797b881fb894ee7fa710e13235aad6cddcc2d9
Rakshit <rakshitpatel910@gmail.com>
2025-02-28 16:14:11 +0530

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,6 +24,7 @@ public class Main {
             System.out.println("4. Power Function (x^b)");
             System.out.println("5. Exit");
             System.out.print("Enter your choice (1-5): ");
+            System.out.flush();
 
             int choice = scanner.nextInt();
 
